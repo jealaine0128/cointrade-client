f1b8bf4852c58ab87325f7833377a5a8e76481a9
jealaine0128 <125180585+jealaine0128@users.noreply.github.com>
2023-08-23 23:06:41 +0800

    Update UserCoins.tsx

--- src/components/profile/UserCoins.tsx
+++ src/components/profile/UserCoins.tsx
@@ -56,13 +56,14 @@ const UserCoins: React.FC<Props> = ({ coins, skeleton, token, userCoins, searchQ
                 </thead>
                 <tbody>
                     {coins.length > 0 ? coins.map(item => (
-                        <tr className="border-b text-slate-200 bg-slate-800 border-gray-700 text-xs" key={item.coin}>
-                            <th scope="row" className="px-6 py-3 font-medium flex items-center gap-3 whitespace-nowrap text-white">
+item.amount !== 0 && (
+                        <tr className="bg-white border-b text-slate-200 dark:bg-slate-800 dark:border-gray-700 text-xs" key={item.coin}>
+                            <th scope="row" className="px-6 py-3 font-medium text-gray-900 flex items-center gap-3 whitespace-nowrap dark:text-white">
                                 <img src={item.image} alt={item.coin} width={30} height={30} />
                                 <div className='text-white flex items-center w-20'>{item.coin}</div>
                             </th>
                             <td className="px-6 py-3">
-                                <div className='h-4 w-24'>
+                                <div className='h-4 w-20'>
                                     {item.price}
                                 </div>
                             </td>
@@ -72,8 +73,8 @@ const UserCoins: React.FC<Props> = ({ coins, skeleton, token, userCoins, searchQ
                                 </div>
                             </td>
                             <td className="px-6 py-3">
-                                <div className='h-4 w-36'>
-                                    $ {item.value.toFixed(4)}
+                                <div className='h-4 w-32'>
+                                    $ {item.value.toFixed(6)}
                                 </div>
                             </td>
                             <td className="px-6 py-3">
@@ -82,6 +83,7 @@ const UserCoins: React.FC<Props> = ({ coins, skeleton, token, userCoins, searchQ
                                 </ul>
                             </td>
                         </tr>
+                        )
                     ))
                         : userCoins.length < 1 && token ?
                             <tr className="border-b text-slate-200 bg-gray-800 border-gray-700">
